b5cee978fc1b685b35af95b68a18f7c8bd9e46c7
Devin <wahmeng@hotmail.com>
2022-10-14 21:51:23 +0800

    Add url state, default submit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // 👇️ prevent page refresh
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter URL to shorten:
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
