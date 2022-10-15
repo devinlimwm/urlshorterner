625d056fb7c84d179459c6f34b30fece24887d1a
Devin <wahmeng@hotmail.com>
2022-10-15 23:35:32 +0800

    Refactor magic number

--- backend/backend/short/services.py
+++ backend/backend/short/services.py
@@ -1,11 +1,13 @@
 import uuid
 from urllib.parse import urlparse
 
+UUID_MAX_LENGTH = 8
+
 
 class UuidService():
     def short_uuid():
         id = str(uuid.uuid4())
-        return id[:8]
+        return id[:UUID_MAX_LENGTH]
 
 
 class UrlService():
