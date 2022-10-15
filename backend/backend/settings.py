b6b544d47ab8a63eb375df953b41aaabeb4bc255
Devin <wahmeng@hotmail.com>
2022-10-15 23:12:45 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '_q@_bsfw$r10r*c(ia6mlx5^1k3^yvgtop%qhjvrl$fvg9yu8a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "host.docker.internal",
+    "localhost"
+]
 
 
 # Application definition
@@ -38,12 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.short'
+    'backend.short',
+    'uuid',
+    'corsheaders',
+    'urllib.parse'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -82,7 +89,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
-        'PORT': 5432,
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
@@ -124,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CSRF_COOKIE_SECURE = False
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
