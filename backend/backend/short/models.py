3ded1b238f2f1658664a5b72c5e6ebafac8789e0
Devin <wahmeng@hotmail.com>
2022-10-15 19:08:28 +0800

    Update models

--- backend/backend/short/models.py
+++ backend/backend/short/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from backend.short.services import UuidService
 
 
 class Url(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    short_url = models.CharField(max_length=10)
+    short_url = models.CharField(
+        primary_key=True, unique=True, default=UuidService.shortUuid, max_length=10)
     long_url = models.CharField(max_length=256)
-    visits = models.IntegerField()
+    visits = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.long_url + self.short_url + created_at
+        return self.long_url + self.short_url + self.created_at
 
     class Meta:
         ordering = ['-visits']
